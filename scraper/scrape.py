a07810a978c89d1f6027b866936f04041269598d
jkaram <jamiekaram16@gmail.com>
2020-09-11 00:24:00 -0400

    find password and input password

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -8,11 +8,12 @@ import time
 url = "https://www.instagram.com/"
 browser = webdriver.Chrome(executable_path="/mnt/l/projects/instagram-data/scraper/chromedriver.exe")
 browser.get(url)
-html = browser.page_source
-soup = Soup(html)
+browser.implicitly_wait(5)
 
 username = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+password = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
 username.send_keys("jamie_karam")
+password.send_keys("1234567")
 
 
 
