2053ff21cbfb851ce7d26fbd04372bb935ee9da5
jkaram <jamiekaram16@gmail.com>
2020-09-11 00:20:57 -0400

    find login input and enter username

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -5,8 +5,15 @@ from selenium.webdriver import ActionChains
 import time
 
 
-url = "https://www.instagram.com/itsborocoprunning/"
-browser = webdriver.Firefox(executable_path="/mnt/l/projects/instagram-data/scrapper/geckodriver.exe")
+url = "https://www.instagram.com/"
+browser = webdriver.Chrome(executable_path="/mnt/l/projects/instagram-data/scraper/chromedriver.exe")
 browser.get(url)
 html = browser.page_source
 soup = Soup(html)
+
+username = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+username.send_keys("jamie_karam")
+
+
+
+
