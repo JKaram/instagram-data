82b92d8149248fb831af9cd9c4a5879fb61a8307
Jamie Karam <jamiekaram16@gmail.com>
2020-11-01 22:23:58 -0500

    add click login

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,18 +3,23 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 import time
+import os
+
+
 
 
 url = "https://www.instagram.com/"
-browser = webdriver.Chrome(executable_path="/mnt/l/projects/instagram-data/scraper/chromedriver.exe")
+browser = webdriver.Chrome(executable_path="./scraper/chromedriver.exe")
 browser.get(url)
 browser.implicitly_wait(5)
 
 username = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
 password = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
-username.send_keys("jamie_karam")
-password.send_keys("1234567")
-
-
+login = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
+username.send_keys("")
+time.sleep(1)
+password.send_keys("")
+time.sleep(1)
+login.click()
 
 
